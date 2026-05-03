5f3f430bd8de7d48f5564ae58b91d28f66602156
Osher1624 <65566722+Osher1624@users.noreply.github.com>
2026-05-03 14:26:24 -0400

    Add ecs_sale_product_configurator/models/__init__.py

---
+++ ecs_sale_product_configurator/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import product_category
+from . import product_attribute
+from . import sale_order
+from . import sale_product_attribute
