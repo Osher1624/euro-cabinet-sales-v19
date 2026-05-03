6aab4c2a809c1e87a15d3c5d6723dc94bdad4e80
Osher1624 <65566722+Osher1624@users.noreply.github.com>
2026-05-03 14:26:20 -0400

    Add ecs_sale_product_configurator/__manifest__.py

---
+++ ecs_sale_product_configurator/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    #  Information
+    'name': "ECS - Sale Product Configurator",
+    'version': '16.0.0.1.0',
+    'category': 'Customization',
+    'summary': "Sale Product Category Configurator",
+    'description': """
+        TaskID:4172793
+        The goal of this module is to configure product attributes in the sales configurator
+        as per the globally defined settings.
+    """,
+    # Author
+    'author': 'Odoo PS',
+    'website': 'https://bit.ly/3GLgMwG',
+    'license': 'LGPL-3',
+
+    # Dependency
+    'depends': ['product', 'sale_product_configurator', 'stock_account'],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_catrgory_view.xml',
+        'views/product_attribute_view.xml',
+        'views/sale_order_view.xml',
+        'views/variant_template.xml',
+    ],
+
+    'assets': {
+        'web.assets_backend': [
+            'ecs_sale_product_configurator/static/src/scss/product_attribute.scss',
+        ]
+    },
+    # Other
+    'installable': True,
+}
